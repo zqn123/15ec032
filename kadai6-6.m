72ef8ab9fcb5577c97e1c5b8a037d4afb084a491
zqn123 <34361421+zqn123@users.noreply.github.com>
2017-12-08 13:59:38 +0900

    Add files via upload

---
+++ kadai6-6.m
@@ -0,0 +1,23 @@
+ % 課題６　画像の二値化 
+ % 下記のプログラムを参考にして画像を二値化せよ． 
+ % 下記はサンプルプログラムである． 
+ % 課題作成にあたっては「Lenna」以外の画像を用いよ． 
+ 
+ 
+ clear; % 変数のオールクリア 
+ ORG=imread('tomato.jpg'); % 原画像の入力 
+ ORG = rgb2gray(ORG); 
+ imagesc(ORG); colormap(gray); colorbar; % 画像の表示 
+ pause; % 一時停止 
+ 
+ 
+ 
+ 
+ IMG = ORG>128; % 128による二値化 
+ imagesc(IMG); colormap(gray); colorbar; % 画像の表示 
+ pause; 
+ 
+ 
+ IMG = dither(ORG); % ディザ法による二値化 
+ imagesc(IMG); colormap(gray); colorbar; % 画像の表示 
+  

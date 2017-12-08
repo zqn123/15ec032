cdce313314a8f92bc1c61a5ec4393b90ecaf842a
zqn123 <34361421+zqn123@users.noreply.github.com>
2017-12-08 13:48:32 +0900

    Add files via upload

---
+++ kadai4-4.m
@@ -0,0 +1,18 @@
+% 課題４　画像のヒストグラム
+% 画素の濃度ヒストグラムを生成せよ．
+% 下記はサンプルプログラムである．
+% 課題作成にあたっては「Lenna」以外の画像を用いよ．
+
+clear; % 変数のオールクリア
+
+ORG=imread('tomato.jpg'); % 原画像の入力
+ORG=rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換
+imagesc(ORG); colormap(gray); colorbar;
+pause;
+
+imhist(ORG); % ヒストグラムの表示
+
+
+
+
+
